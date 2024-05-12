fbead0afc40459b630665befd786f17db469701c
Larissa Matos <63737673+MatosLarissa@users.noreply.github.com>
2024-05-12 07:40:45 -0300

    🎉 chore: initial setting

---
+++ src/main/java/com/pacto/internalrecruitment/InternalRecruitmentApplication.java
@@ -0,0 +1,12 @@
+package com.pacto.internalrecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalRecruitmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternalRecruitmentApplication.class, args);
+    }
+}
