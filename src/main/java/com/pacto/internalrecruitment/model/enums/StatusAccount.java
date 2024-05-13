be0aabb4374278d468fdc36f7cf7a235748f3cc1
MatosLarissa <larissamatos88@outlook.com>
2024-05-12 23:51:49 -0300

    :label: feat: Adicionar enum StatusAccount

--- src/main/java/com/pacto/internalrecruitment/model/enums/StatusAccount.java
+++ src/main/java/com/pacto/internalrecruitment/model/enums/StatusAccount.java
@@ -3,16 +3,14 @@ package com.pacto.internalrecruitment.model.enums;
 import lombok.Getter;
 
 @Getter
-public enum Status {
+public enum StatusAccount {
     PENDING("pending"),
     ACTIVE("active"),
-    REJECTED("rejected"),
-    OPEN("open"),
-    CLOSED("closed");
+    REJECTED("rejected");
 
     private final String value;
 
-    Status(String value) {
+    StatusAccount(String value) {
         this.value = value;
     }
 }
