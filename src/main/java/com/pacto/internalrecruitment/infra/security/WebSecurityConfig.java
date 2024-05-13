a18d436d366c33d2951b989749baa42d13c98b90
Larissa Matos <63737673+MatosLarissa@users.noreply.github.com>
2024-05-13 12:36:31 -0300

    :recycle: refactor: Atualização do método securityFilterChain para adicionar autorização por roles específicas

--- src/main/java/com/pacto/internalrecruitment/infra/security/WebSecurityConfig.java
+++ src/main/java/com/pacto/internalrecruitment/infra/security/WebSecurityConfig.java
@@ -25,12 +25,14 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return  httpSecurity
+        return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .antMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .antMatchers(HttpMethod.GET, "/admin/**").hasAuthority("ROLE_ADMIN")
+                        .antMatchers(HttpMethod.GET, "/user/**").hasAuthority("ROLE_USER")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
