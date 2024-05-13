bea1b3be269a21884a952593fa6c336efde60abd
MatosLarissa <larissamatos88@outlook.com>
2024-05-13 00:53:32 -0300

    :card_file_box: raw: Adição de método findByRoleType no RoleRepository

--- src/main/java/com/pacto/internalrecruitment/repository/RoleRepository.java
+++ src/main/java/com/pacto/internalrecruitment/repository/RoleRepository.java
@@ -1,13 +1,13 @@
 package com.pacto.internalrecruitment.repository;
 
+import com.pacto.internalrecruitment.model.Role;
 import com.pacto.internalrecruitment.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository extends JpaRepository<User, Integer> {
-
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByRoleType(String roleType);
 }
