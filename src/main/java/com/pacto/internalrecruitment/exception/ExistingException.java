88184939eebf8e6e97f5a8bf4418a3ed370cb252
MatosLarissa <larissamatos88@outlook.com>
2024-05-13 00:05:02 -0300

    :boom: feat: Adicionar exceção ExistingException

--- src/main/java/com/pacto/internalrecruitment/exception/ExistingException.java
+++ src/main/java/com/pacto/internalrecruitment/exception/ExistingException.java
@@ -1,2 +1,12 @@
-package com.pacto.internalrecruitment.exception;public class ExistingException {
+package com.pacto.internalrecruitment.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class ExistingException extends RuntimeException {
+
+    public ExistingException(String message) {
+        super(message);
+    }
 }
