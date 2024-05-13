1287e25709bb87a5c401c10ab04fa82f1955ce3c
Larissa Matos <63737673+MatosLarissa@users.noreply.github.com>
2024-05-13 14:59:43 -0300

    :recycle: refactor: Refatoração do serviço RoleService para implementar o método createRole e atualizar a lógica de findAllRoles

--- src/main/java/com/pacto/internalrecruitment/service/RoleService.java
+++ src/main/java/com/pacto/internalrecruitment/service/RoleService.java
@@ -1,19 +1,26 @@
 package com.pacto.internalrecruitment.service;
 
+import com.pacto.internalrecruitment.exception.AlreadyExistsException;
 import com.pacto.internalrecruitment.model.Role;
+import com.pacto.internalrecruitment.model.dtos.role.RoleRequestDto;
+import com.pacto.internalrecruitment.model.dtos.role.RoleResponseDto;
+import com.pacto.internalrecruitment.model.factory.RoleFactory;
 import com.pacto.internalrecruitment.repository.RoleRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class RoleService {
-
-    private static final Logger logger = LoggerFactory.getLogger(RoleService.class);
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private final RoleRepository roleRepository;
 
@@ -22,29 +29,21 @@ public class RoleService {
         this.roleRepository = roleRepository;
     }
 
+    public RoleResponseDto createRole(RoleRequestDto data) {
+        Optional<Role> existingRole = roleRepository.findByRoleType(data.getRoleType().toUpperCase());
+
+        if (existingRole.isPresent()) {
+            throw new AlreadyExistsException("Role already exists");
+        }
+
+        Role newRole = RoleFactory.createRole(data);
+        roleRepository.save(newRole);
+        return new RoleResponseDto(newRole.getId(), newRole.getRoleType());
+    }
+
+
     public List<Role> findAllRoles() {
         logger.info("Finding all roles");
         return roleRepository.findAll();
     }
-
-    public Role findRoleById(Integer id) {
-        return roleRepository.findById(id).orElse(null);
-    }
-    public Role findByRoleType(String roleType) {
-        return roleRepository.findByRoleType(roleType).orElse(null);
-    }
-
-    public Role saveRole(Role role) {
-        Optional<Role> roleExist = roleRepository.findByRoleType(role.getRoleType());
-        return roleRepository.save(role);
-    }
-
-    public Role updateRole(Role role) {
-        return roleRepository.save(role);
-    }
-
-    public void deleteRoleById(Integer id) {
-        roleRepository.deleteById(id);
-    }
-
 }
