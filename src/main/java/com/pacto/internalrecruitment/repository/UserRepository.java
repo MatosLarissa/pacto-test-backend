7d19f34d527420cc7bfed630c71c84a80aad8b85
MatosLarissa <larissamatos88@outlook.com>
2024-05-13 00:10:43 -0300

    :label: feat: Adicionar interface UserRepository

--- src/main/java/com/pacto/internalrecruitment/repository/UserRepository.java
+++ src/main/java/com/pacto/internalrecruitment/repository/UserRepository.java
@@ -1,2 +1,15 @@
-package com.pacto.internalrecruitment.repository;public class UserRepository {
+package com.pacto.internalrecruitment.repository;
+
+import com.pacto.internalrecruitment.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    UserDetails findUserDetailsByEmail(String email);
+
 }
