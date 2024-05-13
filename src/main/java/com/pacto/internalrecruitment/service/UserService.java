9623b31e4da1982c122c0d5dc8b5f5a16790d463
MatosLarissa <larissamatos88@outlook.com>
2024-05-13 00:13:34 -0300

    :label: feat: Adicionar classe UserService

--- src/main/java/com/pacto/internalrecruitment/service/UserService.java
+++ src/main/java/com/pacto/internalrecruitment/service/UserService.java
@@ -1,2 +1,47 @@
-package com.pacto.internalrecruitment.service;public class UserService {
+package com.pacto.internalrecruitment.service;
+
+import com.pacto.internalrecruitment.model.User;
+import com.pacto.internalrecruitment.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService  {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAllUsers() {
+        logger.info("Finding all users");
+        return userRepository.findAll();
+    }
+
+    public User findUserById(Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User saveUser(User user) {
+        Optional<User> userExist = userRepository.findByEmail(user.getEmail());
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Integer id) {
+        userRepository.deleteById(id);
+    }
+
 }
