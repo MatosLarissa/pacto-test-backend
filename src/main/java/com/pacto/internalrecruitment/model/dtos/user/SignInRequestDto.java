71310ec554606effc278ad7698a808a3419e6086
MatosLarissa <larissamatos88@outlook.com>
2024-05-12 23:58:10 -0300

    :label: feat: Adicionar classe SignInRequestDto

--- src/main/java/com/pacto/internalrecruitment/model/dtos/user/SignInRequestDto.java
+++ src/main/java/com/pacto/internalrecruitment/model/dtos/user/SignInRequestDto.java
@@ -1,23 +1,25 @@
-package com.pacto.internalrecruitment.model.dto.user;
+package com.pacto.internalrecruitment.model.dtos.user;
 
-import com.pacto.internalrecruitment.model.enums.user.UserRole;
-import com.pacto.internalrecruitment.shared.enums.Experience;
+import com.pacto.internalrecruitment.model.enums.YearsExperience;
 import lombok.Data;
 
 @Data
 public class SignInRequestDto {
-    private String name;
+    private String firstName;
+
+    private String lastName;
 
     private String email;
 
     private String password;
 
-    private String experienceId;
+    private YearsExperience yearsExperience;
 
-    public SignInRequestDto(String name, String email, String password, String experienceId ){
-        this.name = name;
+    public SignInRequestDto(String firstName, String lastName, String email, String password, YearsExperience yearsExperience ){
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
-        this.experienceId = experienceId;
+        this.yearsExperience = yearsExperience;
     }
 }
