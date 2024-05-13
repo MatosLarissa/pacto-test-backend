c7ade7072014cc9e559656d8c54ac513a39cb0c1
MatosLarissa <larissamatos88@outlook.com>
2024-05-13 00:06:54 -0300

    :label: feat: Adicionar classe HttpResponseCreator

--- src/main/java/com/pacto/internalrecruitment/controller/util/HttpResponseCreator.java
+++ src/main/java/com/pacto/internalrecruitment/controller/util/HttpResponseCreator.java
@@ -1,2 +1,53 @@
-package com.pacto.internalrecruitment.controller.util;public class HttpResponseCreator {
+package com.pacto.internalrecruitment.controller.util;
+
+import com.pacto.internalrecruitment.model.dtos.user.error.HttpErrorReply;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public abstract class HttpResponseCreator
+{
+    protected ResponseEntity<Void> OkResponse()
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    protected <T> ResponseEntity<T> OkResponse(T value)
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(value);
+    }
+
+    protected <T> ResponseEntity<T> CreatedResponse(T value)
+    {
+        return ResponseEntity.status(HttpStatus.CREATED).body(value);
+    }
+
+    protected ResponseEntity<Void> NoContentResponse()
+    {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
+
+    protected ResponseEntity<HttpErrorReply> BadRequestResponse(String[] errors)
+    {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HttpErrorReply(errors));
+    }
+
+    protected ResponseEntity<HttpErrorReply> NotFoundResponse(String[] errors)
+    {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HttpErrorReply(errors));
+    }
+
+    protected ResponseEntity<HttpErrorReply> NotFoundResponse(String error)
+    {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HttpErrorReply(error));
+    }
+
+    protected ResponseEntity<HttpErrorReply> ConflictResponse(String[] errors)
+    {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new HttpErrorReply(errors));
+    }
+
+    protected ResponseEntity<HttpErrorReply> InternalServerErrorResponse(String[] errors)
+    {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new HttpErrorReply(errors));
+    }
 }
