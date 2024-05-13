338adf061d0673618fb3a5c7e535a44406030c75
Larissa Matos <63737673+MatosLarissa@users.noreply.github.com>
2024-05-13 17:40:49 -0300

    :factory: feat: Adicionada RequirementFactory para criar e atualizar instâncias de Requirement

--- src/main/java/com/pacto/internalrecruitment/model/factory/RequirementFactory.java
+++ src/main/java/com/pacto/internalrecruitment/model/factory/RequirementFactory.java
@@ -1,2 +1,23 @@
-package com.pacto.internalrecruitment.model.factory;public class RequirementFactory {
+package com.pacto.internalrecruitment.model.factory;
+
+import com.pacto.internalrecruitment.model.Requirement;
+import com.pacto.internalrecruitment.model.dtos.requirement.RequirementRequestDto;
+
+public class RequirementFactory {
+
+    private static void setRequirementFields(Requirement requirement, RequirementRequestDto requestDto) {
+        requirement.setRequirementName(requestDto.getRequirementName().toUpperCase());
+        requirement.setYearsExperience(requestDto.getYearsExperience().toUpperCase());
+    }
+
+    public static Requirement createRequirement(RequirementRequestDto requestDto) {
+        Requirement newRequirement = new Requirement();
+        setRequirementFields(newRequirement, requestDto);
+        return newRequirement;
+    }
+
+    public static Requirement updateRequirement(Requirement existingRequirement, RequirementRequestDto requestDto) {
+        setRequirementFields(existingRequirement, requestDto);
+        return existingRequirement;
+    }
 }
