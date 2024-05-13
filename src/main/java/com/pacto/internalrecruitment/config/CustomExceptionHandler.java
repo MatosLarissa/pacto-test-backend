69ec0c92d3d1728d4c21849504d13eaad6fbc54b
MatosLarissa <larissamatos88@outlook.com>
2024-05-13 00:15:40 -0300

    :label: feat: Adicionar classe CustomExceptionHandler

--- src/main/java/com/pacto/internalrecruitment/config/CustomExceptionHandler.java
+++ src/main/java/com/pacto/internalrecruitment/config/CustomExceptionHandler.java
@@ -1,2 +1,41 @@
-package com.pacto.internalrecruitment.config;public class CustomExceptionHandler {
+package com.pacto.internalrecruitment.config;
+
+import com.pacto.internalrecruitment.exception.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleBadRequestException(AccessDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<?> handleUserNotFoundException(AlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ExistingException.class)
+    public ResponseEntity<?> handleEmailAlreadyExistsException(ExistingException ex) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InternalServerError.class)
+    public ResponseEntity<?> handleUserUnauthorizedException(InternalServerError ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleJwtExpiredException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+    }
 }
