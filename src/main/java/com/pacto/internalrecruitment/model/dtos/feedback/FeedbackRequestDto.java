bebf5ec47dab66150d0ccda40293e0a77a832ee1
Larissa Matos <63737673+MatosLarissa@users.noreply.github.com>
2024-05-14 14:19:53 -0300

    :sparkles: feat: Adição das classes DTO FeedbackRequestDto e FeedbackResponseDto

--- src/main/java/com/pacto/internalrecruitment/model/dtos/feedback/FeedbackRequestDto.java
+++ src/main/java/com/pacto/internalrecruitment/model/dtos/feedback/FeedbackRequestDto.java
@@ -1,2 +1,27 @@
-package com.pacto.internalrecruitment.model.dtos.feedback;public class FeedbackRequestDto {
+package com.pacto.internalrecruitment.model.dtos.feedback;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FeedbackRequestDto {
+    private String title;
+    private String content;
+    private Integer userResponseId;
+    private Integer userCandidateId;
+    private Integer jobApplicationId;
+    private String statusApplicant;
+
+    public FeedbackRequestDto(FeedbackRequestDto feedbackRequestDto) {
+        this.title = feedbackRequestDto.getTitle();
+        this.content = feedbackRequestDto.getContent();
+        this.userResponseId = feedbackRequestDto.getUserResponseId();
+        this.userCandidateId = feedbackRequestDto.getUserCandidateId();
+        this.jobApplicationId = feedbackRequestDto.getJobApplicationId();
+        this.statusApplicant = feedbackRequestDto.getStatusApplicant();
+    }
 }
--- src/main/java/com/pacto/internalrecruitment/model/dtos/feedback/FeedbackResponseDto.java
+++ src/main/java/com/pacto/internalrecruitment/model/dtos/feedback/FeedbackResponseDto.java
@@ -1,2 +1,25 @@
-package com.pacto.internalrecruitment.model.dtos.feedback;public class FeedbackResponseDto {
+package com.pacto.internalrecruitment.model.dtos.feedback;
+
+import lombok.Data;
+
+@Data
+public class FeedbackResponseDto {
+    private Integer feedbackId;
+    private String title;
+    private String content;
+    private Integer userResponseId;
+    private Integer userCandidateId;
+    private Integer jobApplicationId;
+    private Integer statusApplicant;
+
+    public FeedbackResponseDto(FeedbackResponseDto feedbackResponseDto) {
+        this.feedbackId = feedbackResponseDto.getFeedbackId();
+        this.title = feedbackResponseDto.getTitle();
+        this.content = feedbackResponseDto.getContent();
+        this.userResponseId = feedbackResponseDto.getUserResponseId();
+        this.userCandidateId = feedbackResponseDto.getUserCandidateId();
+        this.jobApplicationId = feedbackResponseDto.getJobApplicationId();
+        this.statusApplicant = feedbackResponseDto.getStatusApplicant();
+
+    }
 }
