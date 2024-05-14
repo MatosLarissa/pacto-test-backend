58f1e054facd64a3618dcce6f3906398387bfd6c
Larissa Matos <63737673+MatosLarissa@users.noreply.github.com>
2024-05-14 14:21:51 -0300

    :sparkles: feat: Adição do AdminController para operações administrativas de usuários

--- src/main/java/com/pacto/internalrecruitment/controller/AdminController.java
+++ src/main/java/com/pacto/internalrecruitment/controller/AdminController.java
@@ -1,2 +1,47 @@
-package com.pacto.internalrecruitment.controller;public class AdminController {
+package com.pacto.internalrecruitment.controller;
+
+import com.pacto.internalrecruitment.model.User;
+import com.pacto.internalrecruitment.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("admin")
+@CrossOrigin("*")
+public class AdminController {
+    private final UserService userService;
+
+    @Autowired
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.findAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PutMapping("users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody User user) {
+        if (!userService.findUserById(id).getUserId().equals(user.getUserId())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        User updatedUser = userService.updateUser(user);
+        if (updatedUser != null) {
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        userService.deleteUserById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
